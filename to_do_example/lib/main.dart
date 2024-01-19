9c78340ec8c070166748bf954d1da1ae94a3c6da
jazz-ing <rsyjlee@gmail.com>
2024-01-19 15:21:38 +0900

    feat: 패키지 적용 및 테마 색 등 초기 세팅 구현

---
+++ to_do_example/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_example/constant/app_color.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  runApp(const ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter ToDo Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColor.theme),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
