23f635edb270497b1a4cd6271905200ce07770e7
jazz-ing <rsyjlee@gmail.com>
2024-01-19 16:55:20 +0900

    feat: 레포지토리 추상클래스 TodoRepository 구현

---
+++ to_do_example/lib/data/todo_repository.dart
@@ -0,0 +1,8 @@
+import 'package:to_do_example/domain/todo/todo.dart';
+
+abstract class TodoRepository {
+  Future<List<Todo>> getTodoList();
+  Future<void> createTodo(Todo todo);
+  Future<void> deleteTodo(Todo todo);
+  Future<void> updateTodo(Todo todo);
+}
