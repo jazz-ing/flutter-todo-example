a0a1710872ccb990a0f2e8c44571128e12310545
jazz-ing <rsyjlee@gmail.com>
2024-01-21 11:14:35 +0900

    feat: 테스트를 위한 MockLocalTodoRepository 추가

---
+++ to_do_example/lib/data/repositories/mock_local_todo_repository.dart
@@ -0,0 +1,64 @@
+import 'package:to_do_example/data/repositories/todo_repository.dart';
+import 'package:to_do_example/domain/todo/todo.dart';
+
+class MockLocalTodoRepository implements TodoRepository {
+  var todoListData = [
+    Todo(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      title: 'Todo 1',
+      isDone: false,
+    ),
+    Todo(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      title: 'Todo 2',
+      isDone: false,
+    ),
+    Todo(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      title: 'Todo 3',
+      isDone: false,
+    ),
+    Todo(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      title: 'Todo 4',
+      isDone: false,
+    ),
+    Todo(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      title: 'Todo 5',
+      isDone: false,
+    ),
+  ];
+
+  @override
+  Future<List<Todo>> getTodoList() async {
+    return todoListData;
+  }
+
+  @override
+  Future<void> createTodo(Todo todo) async {
+    todoListData.add(todo);
+    return;
+  }
+
+  @override
+  Future<void> deleteTodo(Todo todo) async {
+    todoListData.remove(todo);
+    return;
+  }
+
+  @override
+  Future<void> updateTodo(Todo todo) async {
+    final oldTodo = todoListData.firstWhere((t) => t.id == todo.id);
+    final index = todoListData.indexOf(oldTodo);
+    todoListData[index] = todo;
+    return;
+  }
+
+  @override
+  Future<void> reorderTodo(int oldIndex, int newIndex) async {
+    final oldItem = todoListData.removeAt(oldIndex);
+    todoListData.insert(newIndex, oldItem);
+    return;
+  }
+}
